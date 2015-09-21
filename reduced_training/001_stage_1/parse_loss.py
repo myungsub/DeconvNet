8171736e5b999601d6ba87656f649bd4a04d66a1
myungsub <cms6539@gmail.com>
2015-09-22 04:21:37 +0900

    fix loss graph script

--- reduced_training/001_stage_1/parse_loss.py
+++ reduced_training/001_stage_1/parse_loss.py
@@ -2,10 +2,10 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = open('training_log/caffe.cvlab-ara.mschoi.log.INFO.20150921-221243.7556', 'r')
+f = open('training_log/caffe.cvlab-ara.mschoi.log.INFO.20150922-022356.23789', 'r')
 s = f.read()
 pos = [m.end()+1  for m  in re.finditer('Train net output #0: seg-loss =', s)]
-loss_end = [s[pos[i]:pos[i]+8].find(' ') for i in xrange(len(pos))]
+loss_end = [s[pos[i]:pos[i]+10].find(' ') for i in xrange(len(pos))]
 loss = [float(s[pos[i]:pos[i]+loss_end[i]]) for i in xrange(len(pos))]
 
 x = xrange(len(pos))
